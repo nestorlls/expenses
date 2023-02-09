2401a63eb95a7581d67a7e43e6304eed50196d0b
Paul Llanque <llanquepol@gmail.com>
2023-02-09 13:30:07 -0500

    add example module pattern

--- index.js
+++ index.js
@@ -1,61 +1,34 @@
-renderExpenses(expenses);
+const DOMHandler = function (parentElement) {
+  const parent = document.querySelector(parentElement);
+  if (!parent) throw new Error("Parent not found");
+  return {
+    load(module) {
+      parent.innerHTML = module;
+      module.addListeners();
+    },
+  };
+};
 
-function createExpenseEle(expense) {
-  // create elements
-  const li = document.createElement("li");
-  const expenseDetail = document.createElement("div");
-  const container = document.createElement("div");
-  const category = document.createElement("p");
-  const description = document.createElement("p");
-  const amount = document.createElement("p");
-  const actions = document.createElement("div");
-  const deleteLink = document.createElement("a");
+const App = DOMHandler("#root");
 
-  // Properties of the elements (class, content)
-  li.classList.add("expense");
-  expenseDetail.classList.add("expense__detail");
-  category.classList.add("heading-sx", "bold");
-  category.textContent = expense.category;
-  description.classList.add("content-sm", "gray-300");
-  description.textContent = expense.description;
-  amount.classList.add("content-xl", "bold");
-  amount.textContent = `$${expense.amount}`;
-  actions.classList.add("expense__actions");
-  deleteLink.textContent = "Delete";
-
-  // Build template
-  container.append(category, description);
-  actions.append(deleteLink);
-  expenseDetail.append(container, amount);
-  li.append(expenseDetail, actions);
-
-  // add Event listeners
-  deleteLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    deleteExpense(expense);
-    renderExpenses(expenses);
-  });
-  return li;
+// create modules
+function createHeader() {
+  const template = "<h1>Header jejejej</h1>";
+  const listenHeaderClick = () => {
+    const h1 = document.querySelector("h1");
+    h1.addEventListener("click", () => {
+      console.log("click en el header(h1)");
+    });
+  };
+  return {
+    toString() {
+      return template;
+    },
+    addListeners() {
+      listenHeaderClick();
+    },
+  };
 }
 
-
-// const expense = createExpenseEle(expenses[0]);
-// const expenseList = document.querySelector(".js-expenses");
-// expenseList.append(expense);
-
-function renderExpenses(expenses) {
-  const expenseList = document.querySelector(".js-expenses");
-  expenseList.innerHTML = "";
-  expenses.forEach((expense) => {
-    const expenseElem = createExpenseEle(expense);
-    expenseList.append(expenseElem);
-  });
-}
-
-// // Local Storage
-// const peaple = [{ name: "testino" }, { name: "Juan" }, { name: "Angelo" }];
-// localStorage.setItem("peaple", JSON.stringify(peaple));
-
-// // recuperar datos de local Stogare # JSON.parse convierte en Array[{},{},{}]
-// const peapleFromStorage = JSON.parse(localStorage.getItem("peaple"));
-// console.log(peapleFromStorage);
+const Header = createHeader();
+App.load(Header);
